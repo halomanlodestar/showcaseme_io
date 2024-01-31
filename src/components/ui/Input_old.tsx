c83ef8f99de6b19d245b7dc60869859c28c81d90
halomanlodestar <kunalranarj2005@gmail.com>
2024-01-31 17:29:17 +0530

    migrated to next-auth v5

--- src/components/ui/Input_old.tsx
+++ src/components/ui/Input_old.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  label: string;
+  label?: string;
 }
 
 const Input = (props: InputProps) => {
